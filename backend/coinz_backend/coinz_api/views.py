73968cd348c8c15081992276d7258d3bb702d3cd
Eason Chang <eason@easonchang.com>
2024-03-18 18:25:15 -0600

    Try djangorestframework stuff

--- backend/coinz_backend/coinz_api/views.py
+++ backend/coinz_backend/coinz_api/views.py
@@ -1,6 +1,22 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from coinz_api.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
