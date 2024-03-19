55288862fc794fa93b62bac4d08d6001073ba226
Eason Chang <eason@easonchang.com>
2024-03-18 23:45:25 -0600

    Connect new models to django admin site

--- backend/coinz_backend/coinz_api/admin.py
+++ backend/coinz_backend/coinz_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from coinz_api.models import User, Ledger, Currency, Category, RecurringBill, Transaction, UserSettings, CurrencyConversion
+
+admin.site.register(User)
+admin.site.register(Ledger)
+admin.site.register(Currency)
+admin.site.register(CurrencyConversion)
+admin.site.register(Category)
+admin.site.register(RecurringBill)
+admin.site.register(Transaction)
+admin.site.register(UserSettings)
