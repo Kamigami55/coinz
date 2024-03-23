db54e25bfc549b2dc41435bc6f69c6aa478ac37d
Eason Chang <eason@easonchang.com>
2024-03-23 17:04:10 -0600

    Make django admin site models more readable

--- backend/coinz_backend/coinz_api/models.py
+++ backend/coinz_backend/coinz_api/models.py
@@ -8,6 +8,8 @@ class Ledger(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     users = models.ManyToManyField(User, through='LedgerUser', related_name='ledgers')
+    def __str__(self) -> str:
+        return self.name
 
 class LedgerUser(models.Model):
     ledger = models.ForeignKey(Ledger, on_delete=models.CASCADE)
@@ -19,12 +21,16 @@ class Currency(models.Model):
     abbreviation = models.CharField(max_length=255, unique=True)
     symbol = models.CharField(max_length=255)
     icon = models.CharField(max_length=255)
+    def __str__(self) -> str:
+        return self.abbreviation
 
 class CurrencyConversion(models.Model):
     from_currency = models.ForeignKey(Currency, related_name='conversions_from', on_delete=models.CASCADE)
     to_currency = models.ForeignKey(Currency, related_name='conversions_to', on_delete=models.CASCADE)
     rate = models.FloatField()
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return f"{self.from_currency.abbreviation} to {self.to_currency.abbreviation}"
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -41,6 +47,8 @@ class Category(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return f"{self.icon} {self.name} ({self.type})"
 
 class RecurringBill(models.Model):
     ledger = models.ForeignKey(Ledger, on_delete=models.CASCADE, related_name='recurring_bills')
@@ -54,6 +62,8 @@ class RecurringBill(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return f"{self.name} ({self.amount} {self.currency.abbreviation}) ({self.ledger.name} - {self.category.name}) every {self.interval_days} days from {self.start_date} to {self.end_date}"
 
 class Transaction(models.Model):
     ledger = models.ForeignKey(Ledger, on_delete=models.CASCADE, related_name='transactions')
@@ -65,9 +75,13 @@ class Transaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     recurring_bill = models.ForeignKey(RecurringBill, on_delete=models.SET_NULL, null=True, blank=True, related_name='transactions')
+    def __str__(self) -> str:
+        return f"{self.name} ({self.amount} {self.currency.abbreviation}) ({self.ledger.name} - {self.category.name})"
 
 class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='settings')
     default_currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return f"{self.user.username}'s settings"
