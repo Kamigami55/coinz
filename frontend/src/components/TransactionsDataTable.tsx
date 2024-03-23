4ac1fc2adc0681a473b52cb1a15fa8094b59c4af
Eason Chang <eason@easonchang.com>
2024-03-23 10:07:10 -0600

    Add simple filter, pagination, action to TransactionsDataTable

--- frontend/src/components/TransactionsDataTable.tsx
+++ frontend/src/components/TransactionsDataTable.tsx
@@ -1,15 +1,35 @@
 'use client';
 
 import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
 
+import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { TransactionToDisplay } from '@/lib/helper';
 import { formatDate } from '@/lib/time';
 
 export const columns: ColumnDef<TransactionToDisplay>[] = [
   {
     accessorKey: 'createdAt',
-    header: 'Date',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => formatDate(row.getValue('createdAt')),
   },
   {
@@ -42,6 +62,34 @@ export const columns: ColumnDef<TransactionToDisplay>[] = [
     id: 'ledger',
     accessorFn: ({ ledger }) => ledger.name,
     header: 'Ledger',
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      // const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+            // onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
 
