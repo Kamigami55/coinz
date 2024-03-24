8a7f3b1ec8182c4e1294aec7d142e6a509e3339d
Eason Chang <eason@easonchang.com>
2024-03-23 18:10:12 -0600

    Remove unnecessary console logs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -38,8 +38,6 @@ const NavbarItems = [
 
 export function Navbar() {
   const { data: session, status } = useSession();
-  console.log(session);
-  console.log(status);
 
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
