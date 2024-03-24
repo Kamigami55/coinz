3b9916b619abf28a479170ff743362dd776ed248
Eason Chang <eason@easonchang.com>
2024-03-23 22:33:18 -0600

    Add pie chart report to show transactions by category

---
+++ frontend/src/app/reports/layout.tsx
@@ -0,0 +1,36 @@
+import { ActiveLink } from '@/components/ActiveLink';
+
+const NavbarItems = [
+  {
+    name: 'General',
+    href: '/reports',
+  },
+];
+
+export default function ReportsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col gap-4 p-4 md:gap-8 md:p-10">
+      <div className="mx-auto grid w-full max-w-6xl gap-2">
+        <h1 className="text-3xl font-semibold">Reports</h1>
+      </div>
+      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+        <nav className="grid gap-4 text-sm text-muted-foreground">
+          {NavbarItems.map((item) => (
+            <ActiveLink
+              key={item.name}
+              href={item.href}
+              activeClassName="font-semibold text-primary"
+            >
+              {item.name}
+            </ActiveLink>
+          ))}
+        </nav>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+}
