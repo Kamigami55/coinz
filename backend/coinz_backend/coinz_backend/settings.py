1960ce44f30a07bfdd64f9510d366bede63a4641
Eason Chang <eason@easonchang.com>
2024-03-24 11:12:48 -0600

    Add vercel.json and allow host for backend vercel deployment

--- backend/coinz_backend/coinz_backend/settings.py
+++ backend/coinz_backend/coinz_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lasr(=7ckhu=l#hcwk=b#al(1(l$827bj$t%0noa(*#ber0#hj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
