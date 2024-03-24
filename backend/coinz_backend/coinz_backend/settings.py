5fce7e4450a878424d344a16bf288d574f19d51d
Eason Chang <eason@easonchang.com>
2024-03-24 16:03:43 -0600

    Add python-dotenv for local dev

--- backend/coinz_backend/coinz_backend/settings.py
+++ backend/coinz_backend/coinz_backend/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+from dotenv import load_dotenv
+load_dotenv()  # loads the configs from .env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
