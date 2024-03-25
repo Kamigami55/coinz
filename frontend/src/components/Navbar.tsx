ea27ced79dcaefed88f87e0e30ff681008f99c86
Eason Chang <eason@easonchang.com>
2024-03-25 12:01:26 -0600

    Update navbar logo and remove search bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { CircleUser, Menu, Package2, Search } from 'lucide-react';
+import { Menu } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
 
@@ -14,8 +15,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Input } from '@/components/ui/input';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+
+import Logo from '../../public/logo.png';
 
 const NavbarItems = [
   {
@@ -44,9 +46,9 @@ export function Navbar() {
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="/"
-          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          className="flex items-center gap-2 text-lg font-semibold md:text-base shrink-0"
         >
-          <Package2 className="h-6 w-6" />
+          <Image src={Logo} alt="Coinz Logo" className="h-6 w-6" />
           <span className="sr-only">Coinz</span>
         </Link>
         {NavbarItems.map((item) => (
@@ -75,7 +77,7 @@ export function Navbar() {
               href="#"
               className="flex items-center gap-2 text-lg font-semibold"
             >
-              <Package2 className="h-6 w-6" />
+              <Image src={Logo} alt="Coinz Logo" className="h-6 w-6" />
               <span className="sr-only">Coinz</span>
             </Link>
 
@@ -95,16 +97,7 @@ export function Navbar() {
       </Sheet>
 
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <form className="ml-auto flex-1 sm:flex-initial">
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search products..."
-              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-            />
-          </div>
-        </form>
+        <div className="ml-auto flex-1 sm:flex-initial" />
 
         {status === 'authenticated' ? (
           <DropdownMenu>
